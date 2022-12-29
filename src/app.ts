776598d79db30f3463f751b6d11404428777e643
FasihUllah <fasihbaig85@gmail.com>
2022-12-30 01:00:57 +0500

    Added: compression

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,13 @@ import * as bodyParser from "body-parser";
 
 import * as dotenv from "dotenv";
 import routerAPI from "./routes/router";
+import compression from "compression";
 
 dotenv.config();
 
 const app = express();
 
+app.use(compression())
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
@@ -25,5 +27,5 @@ const port = process.env.PORT;
 app.use('/api', routerAPI);
 
 app.listen(port, () => {
-  console.log(`[server]: Server is listening:${port}`);
+  console.info(`[server]: Server is listening:${port}`);
 });
