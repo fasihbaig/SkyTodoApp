26976d9b5c0f6b59686b94ce5228b7a0d11daea5
FasihUllah <fasihbaig85@gmail.com>
2022-12-30 01:24:59 +0500

    added route for authentication

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,13 @@
 import userRouter from "./user.routes";
+import authRoutes from "./authentication.routes";
 import  express from "express";
 
 const router = express.Router();
 
 // ****** User Routes ******//
 router.use("/users", userRouter);
+
+// ****** Authentication Routes ******//
+router.use("/authentication", authRoutes);
 
 export default router;
