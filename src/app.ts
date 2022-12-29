9374e79686d09e27d8541e3ffcec059e32a88a8f
FasihUllah <fasihbaig85@gmail.com>
2022-12-29 17:12:57 +0500

    update app structure

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,29 @@
-import { Request, Response } from "express";
+import express from "express";
+import { DataBase, UserNS } from "./data-access-layer";
 
-const express = require('express');
-const dotenv = require('dotenv');
+import * as bodyParser from "body-parser";
+
+import * as dotenv from "dotenv";
+import routerAPI from "./routes/router";
 
 dotenv.config();
 
 const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+(async () => {
+  const db = new DataBase();
+  await db.initializeDBConnection();
+})();
+
+app.use('/api', routerAPI);
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`[server]: Server is listening:${port}`);
 });
