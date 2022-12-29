3e47a37b7e8bf6e83766bf95091af24dad7d97aa
FasihUllah <fasihbaig85@gmail.com>
2022-12-29 17:12:07 +0500

    updated async handler

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes";
 
 export const asyncHandler = (handler: ( request: Request, response: Response) => Promise<void>) => {
-    return (request: Request, response: Response) => {
+    return async (request: Request, response: Response) => {
         try {
-            return handler(request, response);
-        } catch (error) {
-            response.send(error);
+            await handler(request, response);
+        } catch (error: any) {
+            if(error && error.statusCode) {
+                response.status(error.statusCode).send(error)
+            }
+            response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
         }
     }
 }
