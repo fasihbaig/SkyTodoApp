adb20b6b99ecf4b2b5c8ec566e88a9fbb2ecc1c1
FasihUllah <fasihbaig85@gmail.com>
2022-12-30 01:24:44 +0500

    updated user model

--- src/data-access-layer/models/User.ts
+++ src/data-access-layer/models/User.ts
@@ -2,12 +2,45 @@ import { Schema, model } from 'mongoose';
 import { UserNS }  from '../model-interfaces';
 
 const userSchema = new Schema<UserNS.UserBaseDocument>({
-  name: { type: String, required: true },
-  username: {type: String, require:  true, unique: true, index: true },
-  email: { type: String, required: true, unique: true, index: true  },
-  password: { type: String, require: true },
-  gender: { type: String, enum: Object.values(UserNS.Gender), required: true},
-  avatar: { type: String, required: false}
+  name: { 
+    type: String, 
+    required: true
+  },
+  username: {
+    type: String, 
+    require:  true, 
+    unique: true, 
+    index: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    index: true  
+  },
+  password: { 
+    type: String, 
+    require: true
+  },
+  gender: { 
+    type: String, 
+    enum: Object.values(UserNS.Gender), 
+    required: true
+  },
+  avatar: { 
+    type: String, 
+    required: false
+  },
+  isBlocked: {
+    type: Boolean, 
+    required: false, 
+    default: null
+  },
+  blockedDate: {
+    type: Date, 
+    required: false, 
+    default: null
+  },
 }, {
    timestamps: true 
 });
