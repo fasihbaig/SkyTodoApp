14862dd622fdbad4b3f3e062f10ea034fe257f7d
FasihUllah <fasihbaig85@gmail.com>
2022-12-29 17:10:24 +0500

    Updated user service to add and get user

--- src/service/user/user-service.ts
+++ src/service/user/user-service.ts
@@ -2,11 +2,32 @@ import { UserNS, User } from "../../data-access-layer";
 
 export class UserService {
 
+   /**
+    * 
+    * @param { UserNS.IUser } user 
+    * @returns {Promise<UserNS.UserBaseDocument> }
+    */
    public static createUser(user: UserNS.IUser): Promise<UserNS.UserBaseDocument> {
        return User.create(user);
    }
 
-   public static getUserById(id: string): any {
-        return User.findById({id})
+   /**
+    * 
+    * @param {string} id 
+    * @returns {Promise<UserNS.UserBaseDocument | null>}
+    */
+   public static getUserById(id: string): Promise<UserNS.UserBaseDocument | null> {
+        return User.findById({id}).exec()
    }
+
+   /**
+    * 
+    * @param {UserNS.IUser} user 
+    * @returns {Promise<UserNS.UserBaseDocument[]>}
+    */
+   public static getUsers(user: UserNS.IUser): Promise<UserNS.UserBaseDocument[]> { 
+    return User.find({
+       $op: Object.keys(user).map((key) => ({[key]: user[key as UserNS.IUserKey]}))
+    }).exec();
+}
 }
