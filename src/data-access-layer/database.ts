3cd797531edf53b3982ca45090e4c212156c948b
FasihUllah <fasihbaig85@gmail.com>
2022-12-29 11:12:51 +0500

    Added: database connection service

---
+++ src/data-access-layer/database.ts
@@ -0,0 +1,29 @@
+import { connect, ConnectOptions, Mongoose, MongooseOptions } from "mongoose";
+import { ConnectionOptions } from "tls";
+
+export class DataBase {
+    private databaseConnection: Mongoose | null = null;
+
+    constructor() {}
+
+    public async initializeDBConnection(): Promise<Mongoose> {
+        try {
+            this.databaseConnection = await connect(DataBase.getMongoDBUri(), {
+                dbName: process.env.DB_NAME
+            });
+            this.databaseConnection.set('strictQuery', true);
+            return  this.databaseConnection;
+        } catch (error) {
+            throw Error("Unable to create connection with DB")
+        }
+    }
+
+    /**
+     * 
+     * @returns {string}
+     */
+    private static getMongoDBUri(): string {
+        return `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
+    }
+}
+
