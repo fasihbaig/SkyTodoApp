8b141afc2fa1b73e36a998a4c0a591b319ffab3d
fasihbaig <fasihbaig85@gmail.com>
2023-09-29 06:37:06 -0700

    main module functionality updated

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule, UserModule } from './modules';
+import { DatabaseModule } from './modules/database/database.module';
+import { UserService } from './modules/user/services';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, DatabaseModule],
 })
 export class AppModule {}
