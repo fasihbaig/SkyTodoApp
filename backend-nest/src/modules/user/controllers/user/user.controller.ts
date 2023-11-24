0b83d1af96de1b6aa7ae91a33e086ee529a4c152
fasihbaig <fasihbaig85@gmail.com>
2023-11-25 01:55:46 +0500

    Added: Basic all user get endpoint

--- backend-nest/src/modules/user/controllers/user/user.controller.ts
+++ backend-nest/src/modules/user/controllers/user/user.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserCrudService, UserService } from '../../services';
 
 @Controller('api/user')
 export class UserController {
 
     constructor(
-        private userCrudService: UserCrudService
+        private userCrudService: UserCrudService,
+        private userService: UserService
     ) {}
 
     @Post("create")
     async createUser(@Body() userDTO: any) {
         const user = await this.userCrudService.createUserHandler(userDTO);
         return user;
+    }
+
+    @Get("/")
+    async getAllUsers() {
+        return this.userService.getAllUsers()
     }
 }
--- backend-nest/src/modules/user/services/user/user.service.ts
+++ backend-nest/src/modules/user/services/user/user.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { MONGOOSE, MONGOOSE_DB, User } from '@tm/data-layer';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @Inject(MONGOOSE) private readonly dbLayer: MONGOOSE_DB
+    ) {
+
+    }
+
+    getAllUsers(): Promise<User[]> {
+       return this.dbLayer.models.User.find().exec()
+    }
+}
