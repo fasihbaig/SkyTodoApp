b643d7f58b9c11a4e8530a12e85af136ed8041a7
fasihbaig <fasihbaig85@gmail.com>
2023-11-29 02:36:00 +0500

    added a nest common util folder for general nest task or flows

---
+++ backend-nest/src/nest-common-utils/exception-handler/api-exception-handler.ts
@@ -0,0 +1,22 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+
+@Catch()
+export class ApiExceptionHandler implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal Server Error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.message;
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+    });
+  }
+}
