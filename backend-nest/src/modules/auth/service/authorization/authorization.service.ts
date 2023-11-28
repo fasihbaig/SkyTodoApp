885a44b49d6843aa41028eb664d3ec603ab1d8a4
fasihbaig <fasihbaig85@gmail.com>
2023-11-29 02:29:30 +0500

    Minor updates in user authorization flows

--- backend-nest/src/modules/auth/service/authorization/authorization.service.ts
+++ backend-nest/src/modules/auth/service/authorization/authorization.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
-
-import { MONGOOSE, MONGOOSE_DB  } from "@tm/data-layer";
+import { MONGOOSE, MONGOOSE_DB, User  } from "@tm/data-layer";
+import { TextHashManager } from "@tm/common";
 
 @Injectable()
 export class AuthorizationService {
@@ -9,11 +9,23 @@ export class AuthorizationService {
     ) {}
 
 
-    public processLoginHandler(email: string, password: string) {
+    public async processLoginHandler(email: string, password: string) {
+        const user = await this.getUserByEmail(email);
+        if(!user) {
+            throw new Error("User not found.")
+        }
+
+        const isValidPassword = await TextHashManager.compareHash(user.password, password);
+        if(!isValidPassword) {
+            throw new Error("Invalid User Credentials.")
+        }
+    }
+
+    private getUserByEmail(email: string): Promise< User | null> {
         const { User } = this.dbLayer.models;
         return User.findOne({
             email: {  $eq : email },
-            password: { $eq: password }
-        }, ["id","age", "email", "username"]).exec()
+            isBlocked: { $ne: true }
+        }, ["id","age", "email", "username", "password"]).exec()
     }
 }
