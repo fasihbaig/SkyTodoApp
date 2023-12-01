f166a09031c6a6da0b764f49f876c68c76c71a68
fasihbaig <fasihbaig85@gmail.com>
2023-12-02 02:59:31 +0500

    Fixed minor typo issues in config file

--- backend-nest/src/config.ts
+++ backend-nest/src/config.ts
@@ -1,15 +1,16 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     database: {
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
       name: process.env.DB_USER_NAME,
       password: process.env.DB_PASSWORD,
       dbName: process.env.DB_NAME
     },
     auth: {
         passwordSaltRound: parseInt(process.env.PASSWORD_SALT_ROUND, 10)  || 10,
-        jwtSecret: process.env.JWT_SECRET
+        jwtSecret: process.env.JWT_SECRET,
+        jwtTokenExpiryTimeSec :process.env.JWT_TOKEN_EXPIRY_TIME_SEC
     }
   } as GLOBAL_CONFIG
 );
