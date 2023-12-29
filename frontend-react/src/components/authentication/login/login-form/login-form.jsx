f1fc285dff0ca9dbeda1db4cc7063caa7f037f14
fasihbaig <fasihbaig85@gmail.com>
2023-12-29 14:34:02 +0500

    frontend minor updates

---
+++ frontend-react/src/components/authentication/login/login-form/login-form.jsx
@@ -0,0 +1,68 @@
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+
+const LoginForm = () => {
+    return (
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar>
+
+            <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+        </Box>
+    )
+}
+
+export default LoginForm;
--- frontend-react/src/components/authentication/login/login-page.jsx
+++ frontend-react/src/components/authentication/login/login-page.jsx
@@ -1,9 +1,30 @@
-import "./login.css"
+import "./login.css";
+import LoginForm from "./login-form/login-form"
+import { Box, Container } from "@mui/material";
 
 const LoginPage = () => {
-    return <div>
-        Login Page
-    </div>
+    return  <Container
+             maxWidth="md"
+             sx={{
+                display: "flex",
+                justifyContent: "space-evenly"
+             }}    
+            >
+            <Box>
+                <div>
+                    <img src="/assets/logo/logo-color.svg" width="60%" />
+                </div>
+            </Box>
+            <Box
+                   sx={{
+                    minWidth:"300px",
+                    marginTop: "30px"
+                }}
+            >
+                    <LoginForm/>
+            </Box>
+                
+        </Container>
 }
 
 export default LoginPage;
