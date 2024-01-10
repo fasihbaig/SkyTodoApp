fcc2907f4408d89490ee580f4cef35fa57101a14
fasihbaig <fasihbaig85@gmail.com>
2024-01-10 19:06:48 +0500

    Added responsiveness and logo

--- react-frontend-ts/src/components/nav/Nav.tsx
+++ react-frontend-ts/src/components/nav/Nav.tsx
@@ -6,20 +6,18 @@ import AccountsMenu from './AccountsMenu';
 function Nav() {
     return (
         <nav className=' bg-slate-900  flex flex-col md:flex-row md:justify-between'>
-            <div className='flex flex-col md:flex-row md:justify-start'>
+            <div className='flex flex-col md:flex-row md:justify-start w-full'>
                 <div className='flex justify-start'>
                     <img
-                        className="h-12 w-auto py-2 px-2"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        className="h-14 md:h-12 w-auto py-2 px-2 text-white"
+                        src="/public/logo-no-background.svg"
                         alt="Task Snap"
+                        style={{ fill: 'white', stroke: 'white' }}
                     />
-                    <h2 className="mt-1 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Task Snap
-                    </h2>
                 </div>
-                <div>
-                    <ul className='px-2 ml-2 mt-2'>
-                        <li>TODOs</li>
+                <div className='sm:w-full'>
+                    <ul className='font-bold text-white cursor-pointer h-full flex justify-start items-center w-full md:w-auto'>
+                        <li className="w-full md:w-auto hover:bg-gray-400 px-2 content-center h-12  pt-2 "><span>TODOs</span></li>
                     </ul>
                 </div>
             </div>
