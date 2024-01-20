6d5f7a0ef2456e5de53447dc831f3ebedd448a02
fasihbaig <fasihbaig85@gmail.com>
2024-01-20 14:59:24 +0500

    run prettier on db-layer file

--- backend-nest/src/modules/DB-LAYER/db-layer.module.ts
+++ backend-nest/src/modules/DB-LAYER/db-layer.module.ts
@@ -4,32 +4,32 @@ import { DataBaseConnection, MONGOOSE } from '@tm/data-layer';
 
 @Global()
 @Module({
-    imports: [ ConfigModule ]
+  imports: [ConfigModule]
 })
 export class DbLayerModule {
 
-    static async forRoot() {
-        const dbLayerProvider = {
-            provide: MONGOOSE,
-            useFactory: async ( configService: ConfigService ) => {
-              return DataBaseConnection.getDataLayerProvider(
-                  configService.get("database.host"),
-                  configService.get("database.username"), 
-                  configService.get("database.password"),
-                  configService.get("database.port"),
-                  configService.get("database.authSource"),
-                {
-                    dbName: configService.get("database.dbName")
-                }
-              );
-            },
-            inject: [ ConfigService ],
-          };
+  static async forRoot() {
+    const dbLayerProvider = {
+      provide: MONGOOSE,
+      useFactory: async (configService: ConfigService) => {
+        return DataBaseConnection.getDataLayerProvider(
+          configService.get("database.host"),
+          configService.get("database.username"),
+          configService.get("database.password"),
+          configService.get("database.port"),
+          configService.get("database.authSource"),
+          {
+            dbName: configService.get("database.dbName")
+          }
+        );
+      },
+      inject: [ConfigService],
+    };
 
-        return {
-            module: DbLayerModule,
-            providers: [dbLayerProvider],
-            exports: [ dbLayerProvider ]
-        }
+    return {
+      module: DbLayerModule,
+      providers: [dbLayerProvider],
+      exports: [dbLayerProvider]
     }
+  }
 }
